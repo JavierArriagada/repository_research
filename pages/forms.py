518203eadd61b65b4c4f86468f515a9ce79392e5
JavierArriagada <38988296+JavierArriagada@users.noreply.github.com>
2021-06-21 17:06:04 -0400

    modelo de formulario

---
+++ pages/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Page
+
+class PageForm(forms.ModelForm): #modelo de formulario
+
+    class Meta:
+
+        model = Page
+        fields = ['title', 'content', 'order']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Título'}),
+            'content': forms.Textarea(attrs={'class':'form-control'}),
+            'order': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Orden'}),
+        }
+        labels = {
+            'title':'', 'order':'',
+        }
