1f1508b6cf01327d2a8c70c4e7d5644d7099684f
JavierArriagada <38988296+JavierArriagada@users.noreply.github.com>
2021-06-22 16:05:14 -0400

    recuperacion contraseña

--- repository_research/settings.py
+++ repository_research/settings.py
@@ -131,3 +131,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Auth redirects
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+import os
+#emails
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+else:
+    # aqui hay que configurar un mail real para produccion
+    pass
