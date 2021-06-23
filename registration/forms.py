ccba21eb4309e20e9f837bf0969a31c92a2e291a
JavierArriagada <38988296+JavierArriagada@users.noreply.github.com>
2021-06-23 15:23:36 -0400

    Clase formulkario perfil, template img

--- registration/forms.py
+++ registration/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import  UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile 
 
 class UserCreationFormWithEmail(UserCreationForm):
     email = forms.EmailField(required=True, help_text='Requerido, 254 caracteres como maximo y debe ser valido')
@@ -14,3 +15,13 @@ class UserCreationFormWithEmail(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('el email ya esta registrado, prueba con otro.')
         return email
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio', 'link']
+        widgets = {
+            'avatar': forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
+            'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografía'}),
+            'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
+        }
