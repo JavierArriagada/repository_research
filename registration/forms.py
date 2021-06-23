10f49a7e21cb83eb3ebc7cb2601d585be8dd11df
JavierArriagada <38988296+JavierArriagada@users.noreply.github.com>
2021-06-23 15:58:03 -0400

    agregar opcion de editar email

--- registration/forms.py
+++ registration/forms.py
@@ -17,7 +17,9 @@ class UserCreationFormWithEmail(UserCreationForm):
         return email
 
 class ProfileForm(forms.ModelForm):
+
     class Meta:
+
         model = Profile
         fields = ['avatar', 'bio', 'link']
         widgets = {
@@ -25,3 +27,18 @@ class ProfileForm(forms.ModelForm):
             'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografía'}),
             'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
         }
+
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text='Requerido, 254 caracteres como maximo y debe ser valido')
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if 'email' in self.cleaned_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('el email ya esta registrado, prueba con otro.')
+        return email
