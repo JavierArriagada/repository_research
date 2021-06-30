0c18c4f2243a41f844ad784b20bbb9dd6d8e6db9
JavierArriagada <38988296+JavierArriagada@users.noreply.github.com>
2021-06-30 12:12:43 -0400

    agregada app profiles

--- repository_research/urls.py
+++ repository_research/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
+from profiles.urls import profiles_patterns
 from django.conf import settings
 
 urlpatterns = [
@@ -23,6 +24,8 @@ urlpatterns = [
     path('', include('core.urls')),
     #Path Pages
     path('pages/', include(pages_patterns)),
+    # Paths de profiles
+    path('profiles/', include(profiles_patterns)),
     
     path('admin/', admin.site.urls),
 
